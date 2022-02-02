d59e63b47bfe6f4639da49c67a6f58bba8220faa
Bhaskar02 <bhaskarrao02@gmail.com>
2022-02-02 14:02:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import cv2
+#import cv2
 import datetime
 from glob import glob
 import numpy as np
@@ -11,7 +11,7 @@ import leafmap.foliumap as leafmap
 from PIL import Image
 
 option = st.selectbox('Select the year',('2010', '2011', '2012','2013','2014','2015','2016'))
-a = Image.open('https://github.com/Bhaskar02/dengueapp/blob/main/data/images/2011.PNG')
+a = Image.open('https://github.com/Bhaskar02/dengueapp/blob/main/data/images/2011.png')
 
 #st.image(a, caption='corelation '+option,width=500,use_column_width=500)
 #images = ['output/2010.png', 'output/2011.png', 'output/2012.png']
