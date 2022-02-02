cd0f466b84ccc3937cb19b5cb26ac5eab86416af
Bhaskar02 <bhaskarrao02@gmail.com>
2022-02-02 13:34:55 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+import streamlit as st
+
+from PIL import Image
+
+option = st.selectbox('Select the year',('2010', '2011', '2012','2013','2014','2015','2016'))
+image = Image.open('output/'+option+'.png')
+
+st.image(image, caption='corelation '+option,width=500,use_column_width=500)
+#images = ['output/2010.png', 'output/2011.png', 'output/2012.png']
+#st.image(images, use_column_width=True, caption=["some generic text"] * len(images))
