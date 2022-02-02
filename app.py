70ec56b3f23cae8e29f29bca6422f5d2ac82d6be
Bhaskar02 <bhaskarrao02@gmail.com>
2022-02-02 14:34:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from streamlit.elements.image import image_to_url, MAXIMUM_CONTENT_WIDTH
 from PIL import Image
 
 img = 'https://raw.githubusercontent.com/Bhaskar02/dengueapp/main/data/images/clip_image005.jpg'
-st.image(image)
+st.image(img)
