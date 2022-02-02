616b3dea3c8781cebb6324e7816450c923141291
Bhaskar02 <bhaskarrao02@gmail.com>
2022-02-02 13:56:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ import leafmap.foliumap as leafmap
 from PIL import Image
 
 option = st.selectbox('Select the year',('2010', '2011', '2012','2013','2014','2015','2016'))
-image = 'https://github.com/Bhaskar02/dengueapp/blob/main/data/images/2011.PNG'
+a = Image.open('https://github.com/Bhaskar02/dengueapp/blob/main/data/images/2011.PNG')
 
-st.image(image, caption='corelation '+option,width=500,use_column_width=500)
+#st.image(a, caption='corelation '+option,width=500,use_column_width=500)
 #images = ['output/2010.png', 'output/2011.png', 'output/2012.png']
 #st.image(images, use_column_width=True, caption=["some generic text"] * len(images))
