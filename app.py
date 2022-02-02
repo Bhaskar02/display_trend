cbdc4b64b07dc45cff1b74231b8ec5542b3ece8e
Bhaskar02 <bhaskarrao02@gmail.com>
2022-02-02 14:37:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from PIL import Image
 
 option = st.selectbox('Select the year',('2010', '2011', '2012','2013','2014','2015','2016'))
 
-img = 'https://raw.githubusercontent.com/Bhaskar02/dengueapp/main/data/images/2010.PNG'
+img = 'https://raw.githubusercontent.com/Bhaskar02/dengueapp/main/data/images/'+option+'.PNG'
 st.image(img, caption='corelation '+option,width=500,use_column_width=500)
