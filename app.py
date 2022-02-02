3d3b2a5596a6b2c2c3f6e560b7397935961bedce
Bhaskar02 <bhaskarrao02@gmail.com>
2022-02-02 14:06:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ import pandas as pd
 import streamlit as st
 import leafmap.foliumap as leafmap
 from PIL import Image
-
+def load_image(image_file):
+	img = Image.open(image_file)
+	return img
 option = st.selectbox('Select the year',('2010', '2011', '2012','2013','2014','2015','2016'))
 #a = Image.open('https://github.com/Bhaskar02/dengueapp/blob/main/data/images/2011.png')
 
-st.image('https://github.com/Bhaskar02/dengueapp/blob/main/data/images/2011.png', caption='corelation '+option,width=500,use_column_width=500)
+st.image(load_image('https://github.com/Bhaskar02/dengueapp/blob/main/data/images/2011.png'), caption='corelation '+option,width=500,use_column_width=500)
 #images = ['output/2010.png', 'output/2011.png', 'output/2012.png']
 #st.image(images, use_column_width=True, caption=["some generic text"] * len(images))
