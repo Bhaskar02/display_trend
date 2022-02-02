3500b5c765404ea04821072470ac1d83beec24f1
Bhaskar02 <bhaskarrao02@gmail.com>
2022-02-02 14:24:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
-import datetime
-from glob import glob
-import numpy as np
-import folium
-from folium import plugins
-from folium.plugins import HeatMap
-import pandas as pd
 import streamlit as st
-import leafmap.foliumap as leafmap
+from streamlit.elements.image import image_to_url, MAXIMUM_CONTENT_WIDTH
 from PIL import Image
-import os
-import matplotlib.pyplot as plt
-#def load_image(image_file):
-#	img = Image.open(image_file)
-#	return img
-option = st.selectbox('Select the year',('2010', '2011', '2012','2013','2014','2015','2016'))
-image = df['https://github.com/Bhaskar02/dengueapp/blob/main/data/images/2011.png'].to_string(index=False).lstrip()
-st.image(image)
-#st.image("https://github.com/Bhaskar02/dengueapp/blob/main/data/images/2011.png",width=500,use_column_width=500)
-#st.image(load_image('https://github.com/Bhaskar02/dengueapp/blob/main/data/images/2011.png'), caption='corelation '+option,width=500,use_column_width=500)
-#images = ['output/2010.png', 'output/2011.png', 'output/2012.png']
-#st.image(images, use_column_width=True, caption=["some generic text"] * len(images))
+
+img = st.file_uploader("upload image", type=["jpg", "png"])
+if img:
+    test = Image.open(img)
+    width, height = test.size  # width is needed for image_to_url()
+    if width > MAXIMUM_CONTENT_WIDTH:
+        width = MAXIMUM_CONTENT_WIDTH  # width is capped at 2*730 https://github.com/streamlit/streamlit/blob/949d97f37bde0948b57a0f4cab7644b61166f98d/lib/streamlit/elements/image.py#L39
+    st.image(img)
+    st.write(
+        image_to_url(
+            image=img,
+            width=width,
+            clamp=False,
+            channels="RGB",
+            output_format=img.type,
+            image_id=img.id,  # each uploaded file has a file.id
+        )
+    )
