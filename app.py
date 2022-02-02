621fc02659e66c01e7d69344f7700d5d299260b7
Bhaskar02 <bhaskarrao02@gmail.com>
2022-02-02 14:36:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,7 @@ import streamlit as st
 from streamlit.elements.image import image_to_url, MAXIMUM_CONTENT_WIDTH
 from PIL import Image
 
-img = 'https://raw.githubusercontent.com/Bhaskar02/dengueapp/main/data/images/clip_image005.jpg'
-st.image(img)
+option = st.selectbox('Select the year',('2010', '2011', '2012','2013','2014','2015','2016'))
+
+img = 'https://raw.githubusercontent.com/Bhaskar02/dengueapp/main/data/images/2010.png'
+st.image(img, caption='corelation '+option,width=500,use_column_width=500)
