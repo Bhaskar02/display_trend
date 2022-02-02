e9cef8cf823babecf3c02b66059f1b6c8dcc865b
Bhaskar02 <bhaskarrao02@gmail.com>
2022-02-02 14:22:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ import matplotlib.pyplot as plt
 #	img = Image.open(image_file)
 #	return img
 option = st.selectbox('Select the year',('2010', '2011', '2012','2013','2014','2015','2016'))
-#a = Image.open(os.path.join(r'https://github.com/Bhaskar02/dengueapp/blob/main/data/images/2011.png'))
-#a = Image.open('https://github.com/Bhaskar02/dengueapp/blob/main/data/images/2011.png')
-st.image("https://github.com/Bhaskar02/dengueapp/blob/main/data/images/2011.png",width=500,use_column_width=500)
+image = df['https://github.com/Bhaskar02/dengueapp/blob/main/data/images/2011.PNG'].to_string(index=False).lstrip()
+st.image(image)
+#st.image("https://github.com/Bhaskar02/dengueapp/blob/main/data/images/2011.png",width=500,use_column_width=500)
 #st.image(load_image('https://github.com/Bhaskar02/dengueapp/blob/main/data/images/2011.png'), caption='corelation '+option,width=500,use_column_width=500)
 #images = ['output/2010.png', 'output/2011.png', 'output/2012.png']
 #st.image(images, use_column_width=True, caption=["some generic text"] * len(images))
