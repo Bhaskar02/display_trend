78244f63f8b8eda60ce6cb9f417d19a11d926425
Bhaskar02 <bhaskarrao02@gmail.com>
2022-02-02 13:46:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ import leafmap.foliumap as leafmap
 from PIL import Image
 
 option = st.selectbox('Select the year',('2010', '2011', '2012','2013','2014','2015','2016'))
-image = Image.open('https://github.com/Bhaskar02/display_trend/blob/main/images/'+option+'.png')
+#image = Image.open('https://github.com/Bhaskar02/display_trend/blob/main/images/'+option+'.png')
 
-st.image(image, caption='corelation '+option,width=500,use_column_width=500)
+#st.image(image, caption='corelation '+option,width=500,use_column_width=500)
 #images = ['output/2010.png', 'output/2011.png', 'output/2012.png']
 #st.image(images, use_column_width=True, caption=["some generic text"] * len(images))
