8e3ed05308af3ff3cd793a645193ac1e6a8bb8a9
Bhaskar02 <bhaskarrao02@gmail.com>
2022-02-03 11:47:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit.elements.image import image_to_url, MAXIMUM_CONTENT_WIDTH
 from PIL import Image
-
+import webbrowser
 option = st.selectbox('Select the year',('2010', '2011', '2012','2013','2014','2015','2016','2017','2018'))
 col1, col2 = st.columns(2)
 img = 'https://raw.githubusercontent.com/Bhaskar02/display_trend/main/images/'+option+'.png'
@@ -10,3 +10,9 @@ img1 = 'https://raw.githubusercontent.com/Bhaskar02/display_trend/main/lineplot/
 col1.image(img, caption='corelation '+option, width=450,use_column_width=450)
 #st.image(img1, caption='line'+option,width=400,use_column_width=400)
 col2.image(img1, caption='line  '+option, width=450,use_column_width=450)
+
+
+url = 'https://www.streamlit.io/'
+
+if st.button('Open browser'):
+    webbrowser.open_new_tab(url)
