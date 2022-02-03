91d5955249a003ba01c28081bed961350f85dce6
Bhaskar02 <bhaskarrao02@gmail.com>
2022-02-03 12:15:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ col2.image(img1, caption='line  '+option, width=450,use_column_width=450)
 
 url = 'https://share.streamlit.io/bhaskar02/display_trend/main/app2.py'
 
-if st.button('home'):
+if st.button('home',on_click=url):
     webbrowser.open("https://google.com") 
     #click.launch(url)
