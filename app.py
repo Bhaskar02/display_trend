541a428a5a61bcb3e4d43275f17d388d530d64e0
Bhaskar02 <bhaskarrao02@gmail.com>
2022-02-03 10:01:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from streamlit.elements.image import image_to_url, MAXIMUM_CONTENT_WIDTH
 from PIL import Image
 
 option = st.selectbox('Select the year',('2010', '2011', '2012','2013','2014','2015','2016'))
-
+col1, col2 = st.columns(2)
 img = 'https://raw.githubusercontent.com/Bhaskar02/display_trend/main/images/'+option+'.png'
 img1 = 'https://raw.githubusercontent.com/Bhaskar02/display_trend/main/lineplot/'+option+'.png'
-st.image(img, caption='corelation '+option,width=400,use_column_width=400)
-st.image(img1, caption='line'+option,width=400,use_column_width=400)
+#st.image(img, caption='corelation '+option,width=400,use_column_width=400)
+col1.image(img, caption='corelation '+option,, use_column_width=True)
+#st.image(img1, caption='line'+option,width=400,use_column_width=400)
+col2.image(img1, caption='line  '+option,, use_column_width=True)
