1f71cefb05daad3ae938d154a7400b0c9b83d5bf
Bhaskar02 <bhaskarrao02@gmail.com>
2022-02-03 12:12:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from streamlit.elements.image import image_to_url, MAXIMUM_CONTENT_WIDTH
 from PIL import Image
 import webbrowser
+import click
 option = st.selectbox('Select the year',('2010', '2011', '2012','2013','2014','2015','2016','2017','2018'))
 col1, col2 = st.columns(2)
 img = 'https://raw.githubusercontent.com/Bhaskar02/display_trend/main/images/'+option+'.png'
@@ -15,4 +16,5 @@ col2.image(img1, caption='line  '+option, width=450,use_column_width=450)
 url = 'https://share.streamlit.io/bhaskar02/display_trend/main/app2.py'
 
 if st.button('home'):
-    webbrowser.open(url)
+    #webbrowser.open(url)
+    click.launch(url)
