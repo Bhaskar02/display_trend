473ecf6859f1a7c7f98a5128d7f7ae43f25b115f
Bhaskar02 <bhaskarrao02@gmail.com>
2022-02-03 09:53:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ from PIL import Image
 option = st.selectbox('Select the year',('2010', '2011', '2012','2013','2014','2015','2016'))
 
 img = 'https://raw.githubusercontent.com/Bhaskar02/display_trend/main/images/'+option+'.png'
-img1 = 'https://raw.githubusercontent.com/Bhaskar02/display_trend/main/images/'+option+'.png'
+img1 = 'https://raw.githubusercontent.com/Bhaskar02/display_trend/main/lineplot/'+option+'.png'
 st.image(img, caption='corelation '+option,width=500,use_column_width=500)
+st.image(img1, caption='corelation '+option,width=500,use_column_width=500)
